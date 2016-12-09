f25a7cc791fe9c4e2d3aee8643befd97de853e4d
СергейБеляев <test@test.test>
2016-12-09 10:09:43 +0300

    yandex counter

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -31,6 +31,37 @@ $bg = "background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.3), rg
 </head>
 <body class="fixed-sidebar fixed-topbar fixed-topbar theme-sdtl color-default">
 <?php $this->beginBody() ?>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter41477084 = new Ya.Metrika({
+                    id:41477084,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true,
+                    trackHash:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/41477084" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <div class="wrap">
     <div class="sidebar">
         <div class="logopanel">
